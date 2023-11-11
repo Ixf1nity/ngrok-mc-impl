5fda5bf712551b97ecc8e755b1369d0f51a8136e
Infinity <xa583179@gmail.com>
2023-11-11 13:29:17 +0530

    fixed some things..

--- src/main/java/me/infinity/ngrokcom/NgrokCommunication.java
+++ src/main/java/me/infinity/ngrokcom/NgrokCommunication.java
@@ -1,6 +1,7 @@
 package me.infinity.ngrokcom;
 
 import com.github.alexdlaird.ngrok.NgrokClient;
+import com.github.alexdlaird.ngrok.conf.JavaNgrokConfig;
 import com.github.alexdlaird.ngrok.protocol.CreateTunnel;
 import com.github.alexdlaird.ngrok.protocol.Proto;
 import com.github.alexdlaird.ngrok.protocol.Tunnel;
@@ -14,6 +15,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class NgrokCommunication extends JavaPlugin {
 
     private GatewayDiscordClient client;
+    private NgrokClient ngrokClient;
+
+    private String publicIp;
 
     @Override
     public void onEnable() {
@@ -22,23 +26,27 @@ public final class NgrokCommunication extends JavaPlugin {
                 .login()
                 .block();
 
-        final NgrokClient ngrokClient = new NgrokClient.Builder().build();
-        final CreateTunnel sshCreateTunnel = new CreateTunnel.Builder()
+        this.ngrokClient = new NgrokClient.Builder().build();
+        final CreateTunnel createTunnel = new CreateTunnel.Builder()
                 .withProto(Proto.TCP)
                 .withAddr(25565)
+                .withAuth("NGROK_AUTH_TOKEN")
                 .build();
-        final Tunnel sshTunnel = ngrokClient.connect(sshCreateTunnel);
+        final Tunnel tunnel = ngrokClient.connect(createTunnel);
+        this.publicIp = tunnel.getPublicUrl();
 
         client.getChannelById(Snowflake.of(1172534695813189662L))
                 .ofType(GuildMessageChannel.class)
                 .flatMap(guildMessageChannel -> guildMessageChannel.createMessage(MessageCreateSpec.builder()
-                        .content("* Current connection address: " + sshTunnel.getPublicUrl())
+                        .content("* Current connection address: " + publicIp.replace("tcp://", ""))
                         .build()
                 )).subscribe();
     }
 
     @Override
     public void onDisable() {
+        this.ngrokClient.disconnect(publicIp);
+        this.ngrokClient.kill();
         this.client.logout();
     }
 }
