197360aee3b7ef7c4f44f5b93a5b590056c7f887
Dominik <80042910+tomasalias@users.noreply.github.com>
2023-11-12 16:24:20 +0100

    Update NgrokCommunication.java

--- src/main/java/me/infinity/ngrokcom/NgrokCommunication.java
+++ src/main/java/me/infinity/ngrokcom/NgrokCommunication.java
@@ -6,6 +6,7 @@ import com.github.alexdlaird.ngrok.protocol.CreateTunnel;
 import com.github.alexdlaird.ngrok.protocol.Proto;
 import com.github.alexdlaird.ngrok.protocol.Region;
 import com.github.alexdlaird.ngrok.protocol.Tunnel;
+
 import discord4j.common.util.Snowflake;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
@@ -19,12 +20,13 @@ public final class NgrokCommunication extends JavaPlugin {
 
     private GatewayDiscordClient client;
     private NgrokClient ngrokClient;
-
     private String publicIp;
 
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
+        int ngrokPort = this.getConfig().getInt("NGROK_PORT", 25565); // Default to Minecraft port if not specified
+
         this.client = DiscordClientBuilder.create(Objects.requireNonNull(this.getConfig().getString("BOT_TOKEN")))
                 .build()
                 .login()
@@ -34,13 +36,16 @@ public final class NgrokCommunication extends JavaPlugin {
                 .withAuthToken(this.getConfig().getString("NGROK_AUTH_TOKEN"))
                 .withRegion(Region.valueOf(Objects.requireNonNull(this.getConfig().getString("REGION")).toUpperCase()))
                 .build();
+
         this.ngrokClient = new NgrokClient.Builder()
                 .withJavaNgrokConfig(javaNgrokConfig)
                 .build();
+
         final CreateTunnel createTunnel = new CreateTunnel.Builder()
                 .withProto(Proto.TCP)
-                .withAddr(this.getServer().getPort())
+                .withAddr(ngrokPort) // Use the configured Ngrok port
                 .build();
+
         final Tunnel tunnel = ngrokClient.connect(createTunnel);
         this.publicIp = tunnel.getPublicUrl().replace("tcp://", "");
 
@@ -52,23 +57,21 @@ public final class NgrokCommunication extends JavaPlugin {
                         .build()
                 )).subscribe();
 
-        this.getLogger().info("Listening server on port " + this.getServer().getPort() + ", IP: " + publicIp);
-    }
-    
-    @Override
-    public void onDisable() {
-    try {
-        this.ngrokClient.disconnect(publicIp);
-        this.ngrokClient.kill();
-        // Close any resources related to the NgrokClient, if available
-        // For instance, if there are sockets or connections, close them explicitly
-        // this.ngrokClient.closeResources();
-    } catch (Exception e) {
-        e.printStackTrace();
+        this.getLogger().info("Listening server on port " + ngrokPort + ", IP: " + publicIp);
     }
 
-    if (this.client != null) {
-        this.client.logout().block();
+    @Override
+    public void onDisable() {
+        try {
+            this.ngrokClient.disconnect(publicIp);
+            this.ngrokClient.kill();
+            // this.ngrokClient.closeResources(); // Close any resources
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (this.client != null) {
+            this.client.logout().block();
+        }
     }
-}
 }
